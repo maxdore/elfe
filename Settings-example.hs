2c6ae1d1ec7b2b25b082961752dcc66aa8452bad
Maximilian Dore <maximilian.dore@hexhex.net>
2017-09-04 14:06:58 +0200

    Adds E to settings example

--- Settings-example.hs
+++ Settings-example.hs
@@ -6,7 +6,8 @@ port :: Int
 port = 8000
 
 vampire = Prover "Vampire" "online-atps" ["--atp=vampire", "--only-check"] ["Theorem"] ["No theorem"] ["uns"]
+eprover = Prover "E Prover" "../prover/eprover/PROVER/eprover" ["--cpu-limit=20", "-s", "--auto-schedule"] ["# SZS status Theorem"] ["# SZS status CounterSatisfiable"] ["uns"]
 beagle = Countermodler "BEAGLE" "beagle" [] "Saturated clause set:"
 
-provers = [vampire]
+provers = [vampire, eprover]
 countermodler = [beagle]
